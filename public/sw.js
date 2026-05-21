62af95eb0f9cc2b2f9ccaa442655da977e0e125b
Nitish Srivastava <nitish.srivastava@newfold.com>
2026-05-21 13:40:03 +0530

    made it installable on iphone/android

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'shelftrack-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  './assets/css/app.css',
+  './assets/js/app.js',
+  './assets/js/constants.js',
+  './assets/js/db.js',
+  './assets/js/importExportService.js',
+  './assets/js/inventoryRepository.js',
+  './assets/js/inventoryService.js',
+  './assets/js/uiRenderer.js',
+  './assets/js/utils.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
